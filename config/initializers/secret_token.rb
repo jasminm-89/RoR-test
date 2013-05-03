994c66d11519b39b71fd9895da83d52c4b48c3df
Jasmin <jasmin_m_@hotmail.com>
2013-05-03 17:44:03 +0200

    initial comit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LBE::Application.config.secret_token = 'fee4441f50bcb483a09cc0fc04ad573dc5e21109a8e0569c838464aba5d946830fffb08c5e593d45437117247f8e3f189ecdd4e90af56b84778f9e72a00750c2'
